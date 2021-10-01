62f4251caaf043fdb733be808a9b1ebc9d7ea22d
sabgo <g.sabarinath81@gmail.com>
2021-10-01 20:32:23 +1000

    Add project files.

---
+++ MobileApp/MobileApp/AppDbContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using SQLite;
+using Xamarin.Forms;
+
+namespace MobileApp
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<LocationData> LocationData { get; set; }
+        public DbSet<Setting> Setting { get; set; }
+
+        public AppDbContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(GetDBPath());
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<LocationData>()
+                .Property(e => e.Latitude)
+                .HasConversion<double>();
+            modelBuilder.Entity<LocationData>()
+                .Property(e => e.Longitude)
+                .HasConversion<double>();
+        }
+
+        private string GetDBPath()
+        {
+            var dbFileName = "rscamalertsample.db";
+            var databasePath = Xamarin.Essentials.FileSystem.AppDataDirectory;
+            var fullPath = Path.Combine(databasePath, dbFileName);
+            return $"Data Source={fullPath}";
+        }
+    }
+
+    public class Setting
+    {
+        [PrimaryKey] public int Id { get; set; }
+        public string DistanceInMeters { get; set; }
+        public string ContinuousAlertDistance { get; set; }
+        public bool IsContinuousAlert { get; set; }
+    }
+
+    public class LocationData
+    {
+        public LocationData(Guid id, double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+            Id = id;
+        }
+
+        public Guid Id { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
---
+++ MobileApp/MobileApp/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MobileApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BtnUpdate.Clicked += BtnUpdate_Clicked;
+        }
+
+        private void BtnUpdate_Clicked(object sender, EventArgs e)
+        {
+            SingleInsert(new Setting
+            {
+                Id = 1, DistanceInMeters = "250 meters", ContinuousAlertDistance = "50 meters",
+                IsContinuousAlert = false
+            });
+        }
+
+        private static async void SingleInsert(Setting item)
+        {
+            try
+            {
+                await using var context = new AppDbContext();
+                await context.SingleInsertAsync(item);
+                item.ContinuousAlertDistance = "sabari";
+                await context.SingleUpdateAsync(item);
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", ex.ToString(), "Ok");
+            }
+        }
+    }
+}
